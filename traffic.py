249364b940303b2b4ff0c305ce1709d595d147eb
Michael Pistrang <michael@pistrang.com>
2019-08-12 14:09:40 -0400

    updates for running via cron on mac

--- traffic.py
+++ traffic.py
@@ -2,19 +2,35 @@ import argparse
 import collections
 from datetime import datetime
 import json
+import logging
 import os
 import sys
 
 import dropbox
 from selenium.webdriver import Chrome, ChromeOptions
 
-CHROME_EXECUTABLE_PATH = "chromedriver75"
+CHROME_EXECUTABLE_PATH = "/usr/local/bin/chromedriver75"
+PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+fh = logging.FileHandler(filename=f"{PROJECT_DIR}/logs/traffic.log")
+fh.setLevel(logging.INFO)
+fh.setFormatter(formatter)
+
+log.addHandler(fh)
+
 
 Screenshot = collections.namedtuple("Screenshot", "filename, label, bytes")
 
-
-def get_screenshots(direction_urls, save_to_disk=False):
+def get_screenshots(direction_urls):
     """Yield a generator of Screenshot namedtuples for each provided direction urls"""
+
+    log.info(f"Taking screenshots of {len(direction_urls)} directions.")
+
     now = datetime.now()
 
     chrome_options = ChromeOptions()
@@ -34,39 +50,43 @@ def get_screenshots(direction_urls, save_to_disk=False):
                 )
 
 def get_direction_urls():
-    with open("direction_urls.json") as f:
+    log.info("Parsing direction urls json.")
+
+    with open(f"{PROJECT_DIR}/direction_urls.json") as f:
         direction_urls = json.load(f)
 
     return direction_urls
 
 
 def save_screenshots(screenshots, save_to_disk=False, upload_to_dropbox=False):
-
     dbx = None
     if upload_to_dropbox:
         try:
-            DROPBOX_ACCESS_TOKEN = os.environ["DROPBOX_ACCESS_TOKEN"]
+            DROPBOX_ACCESS_TOKEN="l6v_IxP-c50AAAAAAAAnSQAI1XAhux9-X4szoFZiBg1qA34lE05Fx9J0PIU2Baci"
+            #DROPBOX_ACCESS_TOKEN = os.environ["DROPBOX_ACCESS_TOKEN"]
         except KeyError:
-            print("No DROPBOX_ACCESS_TOKEN found in environment variables!  Skipping the upload to Dropbox.")
+            logging.error("No DROPBOX_ACCESS_TOKEN found in environment variables!  Skipping the upload to Dropbox.")
             upload_to_dropbox = False
         else:
             dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
 
-    for screenshot in screenshots:
+    for i, screenshot in enumerate(screenshots):
+        log.info(f"Saving screenshot {i} to disk.")
         if save_to_disk:
-            filename = f"./traffic/{screenshot.filename}"
+            filename = f"{PROJECT_DIR}/traffic/{screenshot.filename}"
             os.makedirs(os.path.dirname(filename), exist_ok=True)
             try:
                 with open(filename, 'wb') as f:
                     f.write(screenshot.bytes)
             except IOError as e:
-                print(f"Could not write {screenshot.filename} due to {e}.")
+                logging.error(f"Could not write {screenshot.filename} due to {e}.")
 
         if upload_to_dropbox:
+            log.info(f"Saving screenshot {i} to Dropbox.")
             try:
-                dbx.files_upload(screenshot.bytes, f"/traffic/{screenshot.label}/{screenshot.filename}")
+                dbx.files_upload(screenshot.bytes, f"/traffic/{screenshot.filename}")
             except Exception as e:
-                print(f"Could not write {screenshot.filename} due to {e}.")
+                logging.error(f"Could not write {screenshot.filename} due to {e}.")
 
 
 
@@ -78,13 +98,16 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if not args.save_to_disk and not args.upload_to_dropbox:
-        print("Save to disk or upload to dropbox required.  Otherwise what's the point?")
-        sys.exit(1)
+    log.info(f"Traffic.py called with args {args.__dict__}")
 
+    if not args.save_to_disk and not args.upload_to_dropbox:
+        logging.error("Save to disk or upload to dropbox required.  Otherwise what's the point?")
+        sys.exit(1)
 
     direction_urls = get_direction_urls()
 
     screenshots = get_screenshots(direction_urls)
 
     save_screenshots(screenshots, save_to_disk=args.save_to_disk, upload_to_dropbox=args.upload_to_dropbox)
+
+    log.info("Done saving screenshots of trafic.")
